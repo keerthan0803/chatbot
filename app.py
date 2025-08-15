533f2b15d1dd0c6406622b00cf5dca7e48d84601
PENTAM KEERTHAN <23eg106b48@anurag.edu.in>
2025-08-15 12:13:09 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -35,5 +35,4 @@ def handle_submit():
     return jsonify({'response': response})
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True)
