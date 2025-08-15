b25bb9df5c787f9af64b7921a507289b33ae1449
PENTAM KEERTHAN <23eg106b48@anurag.edu.in>
2025-08-15 12:14:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -35,4 +35,5 @@ def handle_submit():
     return jsonify({'response': response})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
